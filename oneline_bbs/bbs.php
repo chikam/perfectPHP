d3f70f07fee77d8f02f6198e8f5c2cab3bfb4f27
chikam <c.coco.102@gmail.com>
2016-05-22 17:05:35 +0900

    投稿フォーム画面作成

---
+++ oneline_bbs/bbs.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>ひとこと掲示板</title>
+</head>
+<body>
+  <h1>ひとこと掲示板</h1>
+
+  <form action="bbs.php" method="post">
+    名前：<input type="text" name="name" /><br />
+    ひとこと：<input type="text" name="comment" size="60" /><br />
+    <input type="submit" name="submit" value="送信">
+  </form>
+
+</body>
+</html>
