68e661df88780c18d21300a1a172f26438a70ad7
chikam <c.coco.102@gmail.com>
2016-05-22 18:04:42 +0900

    修正とファイル分離

--- oneline_bbs/bbs.php
+++ oneline_bbs/bbs.php
@@ -44,64 +44,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
         . date('Y-m-d H:i:s') . "')";
 
     mysql_query($sql, $link);
+    mysql_close($link);
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
   }
 }
 
-?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <title>ひとこと掲示板</title>
-</head>
-<body>
-  <h1>ひとこと掲示板</h1>
-
-  <form action="bbs.php" method="post">
-    <?php if (count($errors)): ?>
-    <ul class="error_list">
-      <?php foreach($errors as $error): ?>
-      <li>
-        <?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?>
-      </li>
-      <?php endforeach; ?>
-    </ul>
-    <?php endif ; ?>
-    名前：<input type="text" name="name" /><br />
-    ひとこと：<input type="text" name="comment" size="60" /><br />
-    <input type="submit" name="submit" value="送信">
-  </form>
-
-  <?php
-  $sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
-  $result = mysql_query($sql, $link);
-  $post = array();
-  if ($result !== false && mysql_num_rows($result))
+$sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
+$result = mysql_query($sql, $link);
+$post = array();
+if ($result !== false && mysql_num_rows($result))
+{
+  while ($post = mysql_fetch_assoc($result))
   {
-    while ($post = mysql_fetch_assoc($result))
-    {
-      $posts[] = $post;
-    }
+    $posts[] = $post;
   }
-  mysql_free_result($result);
-  mysql_close($link);
-  ?>
+}
+mysql_free_result($result);
+mysql_close($link);
 
-  <?php if (count($posts) > 0): ?>
-  <ul>
-    <?php foreach ($posts as $post): ?>
-    <li>
-      <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>：
-      <?php echo htmlspecialchars($post['comment'], ENT_QUOTES, 'UTF-8'); ?>
-      - <?php echo htmlspecialchars($post['created_at'], ENT_QUOTES, 'UTF-8'); ?>
-    </li>
-    <?php endforeach; ?>
-  </ul>
-  <?php endif; ?>
-
-<?php
-  header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+include 'views/bbs_views.php';
 ?>
-
-</body>
-</html>
